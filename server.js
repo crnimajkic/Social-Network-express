7eae35887323c7657a9ecc6d1deb55e70f8f7163
crnimajkic <mlmajkicmlmajkic@gmail.com>
2023-01-14 03:01:07 +0100

    some changes

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const cors = require('cors')
 app.use(express.static(__dirname +'/public'))
 app.use(express.urlencoded({ extended: true }))
 
-app.use(cors())
+app.use(cors({
+    origin:["https://hexa-social-network.onrender.com","https://kit.fontawesome.com/a076d05399.js"]
+}))
 
 
 app.get('/', (req, res) => {
